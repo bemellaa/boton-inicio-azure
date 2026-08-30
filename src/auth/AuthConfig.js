22ec9cc95ad2d0a63bbcf085493a6f0a31ca70a0
bbenjaaa18 <benjamella926gmail.com>
2026-08-29 21:11:28 -0400

    feat: configurar redirect HTTPS del frontend

--- src/auth/AuthConfig.js
+++ src/auth/AuthConfig.js
@@ -2,8 +2,8 @@ export const msalConfig = {
     auth: {
         clientId: "518f7708-c599-49b3-986a-2df6a7e2b830",
         authority: "https://login.microsoftonline.com/631afcf8-65f9-405f-b756-eac2b39633ca",
-        redirectUri: "http://localhost:5173",
-        postLogoutRedirectUri: "http://localhost:5173",
+        redirectUri: "https://95xuwhqaq2.execute-api.us-east-1.amazonaws.com/desarrollo",
+        postLogoutRedirectUri: "https://95xuwhqaq2.execute-api.us-east-1.amazonaws.com/desarrollo",
     },
     cache: {
         cacheLocation: "sessionStorage",
